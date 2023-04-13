05eb579bc149e6ef34b3cf82f12be6d968c78268
Azamat Jumabekov <so.fly.azash@gmail.com>
2023-04-13 18:19:41 +0600

    allow host

--- core/settings.py
+++ core/settings.py
@@ -171,5 +171,6 @@ CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:8080',
-    'http://127.0.0.1:9000',
+    'http://127.0.0.1:8000',
+    'https://emark-group-production.up.railway.app/',
 ]
