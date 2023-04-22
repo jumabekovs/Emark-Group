70df1a5f123a2f5f25f0e811d2b49268b65f2e09
Azamat Jumabekov <so.fly.azash@gmail.com>
2023-04-22 18:28:46 +0600

    uncommited

--- applications/construction_object/urls.py
+++ applications/construction_object/urls.py
@@ -4,6 +4,6 @@ from .views import ConstructionListView, ConstructionDetailView, FlatListView, F
 urlpatterns = [
     # path('list/', ConstructionListView.as_view()),
     # path('<int:pk>/', ConstructionDetailView.as_view()),
-    # path('flat/list/', FlatListView.as_view()),
+    path('flat/list/', FlatListView.as_view()),
     path('flat/<int:pk>/', FlatDetailView.as_view()),
 ]
