7fd89640b426c3713df439b28f5e6fb198e6c068
Azamat Jumabekov <so.fly.azash@gmail.com>
2023-05-17 15:16:21 +0600

    category document

--- applications/document/models.py
+++ applications/document/models.py
@@ -1,7 +1,17 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+class DocumentCategory(models.TextChoices):
+    history = ('история', _('история'))
+    corporate_management = ('корпоративное управление', _('корпоративное управление'))
+    for_investors = ('инвесторам', _('инвесторам'))
+    for_partners = ('партнерам', _('партнерам'))
+    standard_ISO = ('стандарт ISO 9001', _('стандарт ISO 9001'))
 
 
 class Document(models.Model):
+    category = models.CharField(verbose_name='категория', max_length=256, choices=DocumentCategory.choices)
     name = models.CharField(verbose_name='название документа', max_length=256)
     document = models.CharField(verbose_name='ссылка на документ', max_length=1000)
 
