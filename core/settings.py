37a69ccaa95231fe15dcb1c48c298c855a141436
Azamat Jumabekov <so.fly.azash@gmail.com>
2023-06-20 16:27:41 +0600

    page number 100

--- core/settings.py
+++ core/settings.py
@@ -168,7 +168,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 5,
+    'PAGE_SIZE': 100,
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
 }
 
